171c69dd9e5c405ce26e75cf25538995650c946c
river <river@vvl.me>
2019-08-26 20:56:26 +0800

    macro: fix lxcfs_{error,debug,v} build error when __VA_ARGS__ is empty
    Originally, the compiler complained:
      macro.h:7:25: error: expected expression before ')' token
          __func__, __VA_ARGS__);
    
    The reason is that GCC wouldn't abandon `,` when `__VA_ARGS__` is empty.
    For emaple:
      #define eprintf(format, ...) fprintf (stderr, format, __VA_ARGS__)
      eprintf("success!\n", );
           → fprintf(stderr, "success!\n", );
    
    According to GCC doc, it's okay when adding `##` before `__VA_ARGS__`:
      #define eprintf(format, ...) fprintf (stderr, format, ##__VA_ARGS__)
        eprintf ("success!\n")
           → fprintf(stderr, "success!\n");
    
    Signed-off-by: river <river@vvl.me>

--- macro.h
+++ macro.h
@@ -4,13 +4,13 @@
 #define lxcfs_debug_stream(stream, format, ...)                                \
 	do {                                                                   \
 		fprintf(stream, "%s: %d: %s: " format, __FILE__, __LINE__,     \
-			__func__, __VA_ARGS__);                                \
+			__func__, ##__VA_ARGS__);                                \
 	} while (false)
 
-#define lxcfs_error(format, ...) lxcfs_debug_stream(stderr, format, __VA_ARGS__)
+#define lxcfs_error(format, ...) lxcfs_debug_stream(stderr, format, ##__VA_ARGS__)
 
 #ifdef DEBUG
-#define lxcfs_debug(format, ...) lxcfs_error(format, __VA_ARGS__)
+#define lxcfs_debug(format, ...) lxcfs_error(format, ##__VA_ARGS__)
 #else
 #define lxcfs_debug(format, ...)
 #endif /* DEBUG */
